099652eba80b9aa4fa9196f4135c71c8a40753e2
LordSaumya <e1036312@u.nus.edu>
2023-01-17 12:20:30 +0800

    Fixed line error in shell script

--- .fix_crypto_dependency.sh
+++ .fix_crypto_dependency.sh
@@ -2,7 +2,7 @@
 webpack_config="./node_modules/react-scripts/config/webpack.config.js"
 webpack_config_backup="./node_modules/react-scripts/config/webpack.config.js.bckp"
 
-line_number=304
+line_number=306
 line_to_add='fallback: { "crypto": require.resolve("crypto-browserify") }, // Patch realm-web crypto dependency'
 
 echo "Fixing realm-web crypto dependency..."
